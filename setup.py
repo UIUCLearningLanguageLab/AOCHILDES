dfdeb4dc1a64fdf9c1218c9c920a19c7db42e1a4
phueb <phueb001@ucr.edu>
2018-10-08 11:20:39 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CHILDESHub',
+    version='0.1dev',
+    packages=['src', 'analysis'],
+    url='https://github.com/phueb/CHILDESHub',
+    license='',
+    author='Philip Huebner',
+    author_email='',
+    description='Create and analyze variants of American-English CHILDES corpus'
+)
