fb296cd7d702bdc645b503ae9b497b12e5667981
phueb <phueb001@ucr.edu>
2019-03-25 23:27:20 -0500

    small changes to analysis script

--- analysis/analyze_input_output_corr_mat_svd.py
+++ analysis/analyze_input_output_corr_mat_svd.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 from childeshub.hub import Hub
 
 HUB_MODE = 'sem'
-NUM_PCS = 100
-CAT = 'animal'
+NUM_PCS = 10
 DIST = 1
 CORRELATE = False
+NOUNS = False
 
 LEG_FONTSIZE = 16
 AX_FONTSIZE = 16
@@ -18,10 +18,9 @@ FIGSIZE = (20, 6)
 DPI = 200
 
 VERBOSE = False
+PLOT_BY_CAT = True
 
 hub = Hub(mode=HUB_MODE)
-
-assert CAT in hub.probe_store.cats
 
 
 def make_in_out_corr_mat(start, end):
@@ -39,29 +38,38 @@ def make_in_out_corr_mat(start, end):
         for out_loc in out_locs:
             out_term = hub.train_terms.tokens[out_loc]
             col_id = term2type_id[out_term]
-            # cooc_mat[row_id, col_id] += 1
-            cooc_mat[row_id, col_id] = 1
+            cooc_mat[row_id, col_id] += 1
+            # cooc_mat[row_id, col_id] = 1
         pbar.update()
     # corr_mat
     res = cosine_similarity(cooc_mat) if CORRELATE else cooc_mat
     return res, types
 
 
-def calc_loadings_at_each_pc(u, types, cat):
-    if cat == 'noun':
+def calc_metric(type_id2term, words, u_col):
+    """
+    this metric is invented by ph: it is the average rank of probes in a category,
+    where rank is defined by how high a probe loads compared to all other words in vocab for given PC
+    """
+    ranks = []
+    for rank, type_id in enumerate(np.argsort(u_col)):
+        term = type_id2term[type_id]
+        if term in words:
+            ranks.append(rank)
+    return np.mean(ranks)
+
+
+def calc_metric_at_each_pc(u, types, cat):
+    if NOUNS:
         words = hub.nouns
     else:
         words = hub.probe_store.cat_probe_list_dict[cat]
+    #
+    type_id2term = {n: t for n, t in enumerate(types)}
     res = []
     for pc_id, u_col in enumerate(u.T):
-        if VERBOSE:
-            print('principal component={}'.format(pc_id))
-        y_true = np.asarray([1 if t in words else 0 for t in types])
-        y_score = u_col
-        ap = np.dot(y_true, y_score)
-        if VERBOSE:
-            print(cat, np.round(ap, 2))
-        res.append(ap)
+        metric = calc_metric(type_id2term, words, u_col)
+        res.append(metric)
     return res
 
 
@@ -70,22 +78,56 @@ in_out_corr_mat1, types1 = make_in_out_corr_mat(0, hub.midpoint_loc)
 in_out_corr_mat2, types2 = make_in_out_corr_mat(hub.midpoint_loc, hub.train_terms.num_tokens)
 
 
+# fig summarizing across categories
+fig, ax = plt.subplots(1, figsize=FIGSIZE, dpi=DPI)
+plt.title('Loadings on PCs {}\nderived from word co-occurrences at dist={}'.format(
+    'for nouns' if NOUNS else 'across semantic categories', DIST), fontsize=AX_FONTSIZE)
+ax.set_ylabel('Average Rank of Category Probes', fontsize=AX_FONTSIZE)
+ax.set_xlabel('Principal Component', fontsize=AX_FONTSIZE)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.tick_params(axis='both', which='both', top=False, right=False)
+ax.set_ylim([0, len(hub.train_terms.types)])
+ax.axhline(y=len(hub.train_terms.types) / 2, color='grey')
+# plot
+for label, corr_mat, types in [('partition 1', in_out_corr_mat1, types1),
+                               ('partition 2', in_out_corr_mat2, types2)]:
+    # pca
+    pca = PCA(n_components=NUM_PCS)  # = u, s, v
+    u = pca.fit_transform(corr_mat)  # (num_types, NUM_PCS)
+    y = np.zeros(NUM_PCS)
+    for cat in hub.probe_store.cats:
+        y += calc_metric_at_each_pc(u, types, cat)
+        if NOUNS:
+            break
+    #
+    ax.plot(y / hub.probe_store.num_cats, label=label, linewidth=2)
+ax.legend(loc='best', frameon=False, fontsize=LEG_FONTSIZE,
+          bbox_to_anchor=(1.0, 1.0))
+plt.tight_layout()
+plt.show()
+
+if not PLOT_BY_CAT:
+    raise SystemExit
+
+# fig for each category
 for cat in hub.probe_store.cats:
-    # fig
     fig, ax = plt.subplots(1, figsize=FIGSIZE, dpi=DPI)
     plt.title('Do any of the first {} PCs code for the category {}?'.format(NUM_PCS, cat.upper()), fontsize=AX_FONTSIZE)
-    ax.set_ylabel('Abs(Sum(Loadings of {} words))'.format(cat.upper()), fontsize=AX_FONTSIZE)
+    ax.set_ylabel('Average Rank of Category Probes'.format(cat.upper()), fontsize=AX_FONTSIZE)
     ax.set_xlabel('Principal Component', fontsize=AX_FONTSIZE)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.tick_params(axis='both', which='both', top=False, right=False)
+    ax.set_ylim([0, len(hub.train_terms.types)])
+    ax.axhline(y=len(hub.train_terms.types) / 2, color='grey')
     # plot
     for label, corr_mat, types in [('partition 1', in_out_corr_mat1, types1),
                                    ('partition 2', in_out_corr_mat2, types2)]:
         # pca
         pca = PCA(n_components=NUM_PCS)  # = u, s, v
         u = pca.fit_transform(corr_mat)  # (num_types, NUM_PCS)
-        y = np.abs(calc_loadings_at_each_pc(u, types, cat))
+        y = calc_metric_at_each_pc(u, types, cat)
         #
         ax.plot(y, label=label, linewidth=2)
     ax.legend(loc='best', frameon=False, fontsize=LEG_FONTSIZE,
