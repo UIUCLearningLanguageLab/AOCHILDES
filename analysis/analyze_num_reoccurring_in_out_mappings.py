877727d8bfd390290da911cd17f4a8bd6d48afc2
phueb <phueb001@ucr.edu>
2019-04-25 21:53:30 -0500

    simplify script

--- analysis/analyze_num_reoccurring_in_out_mappings.py
+++ analysis/analyze_num_reoccurring_in_out_mappings.py
@@ -4,49 +4,44 @@ import seaborn as sns
 
 from childeshub.hub import Hub
 
-# CORPUS_NAMES = ['childes-20180315', 'childes-20180319']
-P_NOISES = ['no_o', 'all_1', 'all_2', 'all_3', 'all_4']
-WINDOW_SIZE = 2
+WINDOW_SIZES = [2, 3, 4, 5, 6]
+
+hub = Hub(mode='sem')
+windows_mat1 = hub.make_windows_mat(hub.reordered_partitions[0], hub.num_windows_in_part)
+windows_mat2 = hub.make_windows_mat(hub.reordered_partitions[1], hub.num_windows_in_part)
 
 
-def calc_num_shared_io_mappings(p):  # TODO speedup usign just probe windows
-    windows_mat = hub.make_windows_mat(p, hub.num_windows_in_part)[:, -WINDOW_SIZE:]
-
-    u = np.unique(windows_mat, axis=0)
-    result = len(windows_mat) - len(u)
-
-    print(len(windows_mat), len(u), result)
-
+def calc_num_shared_io_mappings(w_mat, w_size):
+    truncated_w_mat = w_mat[:, -w_size:]
+    u = np.unique(truncated_w_mat, axis=0)
+    num_total_windows = len(truncated_w_mat)
+    result = num_total_windows - len(u)
+    #
+    print(len(truncated_w_mat), len(u), result)
     return result
 
 
 # make data
 ys_list = []
-for p_noise in P_NOISES:
-    hub = Hub(mode='sem',
-              num_parts=2,
-              part_order='inc_age',
-              corpus_name='childes-20180315',
-              p_noise=p_noise)
-    y1 = calc_num_shared_io_mappings(hub.reordered_partitions[0])
-    y2 = calc_num_shared_io_mappings(hub.reordered_partitions[1])
+for window_size in WINDOW_SIZES:
+    y1 = calc_num_shared_io_mappings(windows_mat1, window_size)
+    y2 = calc_num_shared_io_mappings(windows_mat2, window_size)
     ys_list.append((y1, y2))
 
 # fig
 bar_width0 = 0.0
 bar_width1 = 0.25
 _, ax = plt.subplots(dpi=192)
-plt.title('window_size={}'.format(WINDOW_SIZE))
 ax.set_ylabel('Number of Re-occurring IO Mappings')
 ax.set_xlabel('punctuation')
-ax.set_xlabel('p_noise')
+ax.set_xlabel('window_size')
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.tick_params(axis='both', which='both', top='off', right='off')
-num_conditions = len(P_NOISES)
+num_conditions = len(WINDOW_SIZES)
 xs = np.arange(1, num_conditions + 1)
 ax.set_xticks(xs)
-ax.set_xticklabels(P_NOISES)
+ax.set_xticklabels(WINDOW_SIZES)
 # plot
 colors = sns.color_palette("hls", 2)[::-1]
 labels = ['partition 1', 'partition 2']
