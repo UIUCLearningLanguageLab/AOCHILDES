cb9724c87a8b4859f4ee574e6faf220552a5d337
phueb <info@philhuebner.com>
2021-04-12 13:23:42 -0500

    move data to src dir to make installable

--- aochildes/configs.py
+++ aochildes/configs.py
@@ -3,5 +3,4 @@ from pathlib import Path
 
 class Dirs:
     src = Path(__file__).parent
-    root = src.parent
-    transcripts = root / 'original_transcripts'
+    transcripts = src / 'original_transcripts'
