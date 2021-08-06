0c4f1f4f092156f48214e760734a525fbc472afd
phueb <info@philhuebner.com>
2021-08-06 12:49:13 -0500

    add script to save raw vocab

---
+++ scripts/get_raw_vocab.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+from aochildes.dataset import ChildesDataSet
+
+data = ChildesDataSet()
+
+words = []
+for _, row in data.pipeline.df.iterrows():
+    sentence = row['gloss']
+    for word in sentence.split():
+        words.append(word)
+
+c = Counter(words)
+
+lines = [f'{w:<36} {f:>9}' for w, f in sorted(c.items(), key=lambda i: i[1], reverse=True)]
+print('\n'.join(lines), file=open('raw_vocab.txt', 'w'))
